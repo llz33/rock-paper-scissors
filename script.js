c9d7d5b99888c8f416c545bdb68ba487f4ca702f
llz33 <noahj8@gmail.com>
2025-06-05 18:30:17 -0700

    Create ability to play one game
    This commit will allow the user to play one round of rock paper
    scissors against the computer before needing to refresh the page.
    
    I learned that my function playRound was actually working the whole
    time, I just needed to put console.log("x") before the return value
    to allow the if statements to work properly.

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
 function getComputerChoice(min, max){
     const maxFloor = Math.floor(max);
     const minCeil = Math.ceil(min);
-    return Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
+    let number = Math.floor(Math.random() * (maxFloor - minCeil + 1) + minCeil);
+    if (number === 1) {
+        console.log("rock");
+        return "rock";
+    }
+    else if (number === 2){
+        console.log("paper");
+        return "paper";
+    }
+    else {
+        console.log("scissors");
+        return "scissors" ;
+    }
 }
 
-switch (getComputerChoice(1, 3)){
-    case 1: console.log("Rock");
-    break;
-    case 2: console.log("Paper");
-    break;
-    case 3: console.log("Scissors");
-    default: "Thinking..."
+
+let getUserChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+
+console.log(getUserChoice);
+
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(humanScore, computerScore)
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+         console.log("Human Wins!")
+        return humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("Computer Wins!")
+        return computerScore++;
+    } else if (humanChoice ==="paper" && computerChoice === "rock") {
+         console.log("Human Wins!")
+        return humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("Computer Wins!")
+        return computerScore++;
+    }  else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("Human Wins!")
+        return humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Computer Wins!")
+        return computerScore++;
+    } else if (humanChoice === computerChoice){
+        console.log("Its a Tie")
+        return "Its a Tie"
+    }
 }
 
+const humanChoice = getUserChoice;
+const computerChoice = getComputerChoice(1, 3);
 
+playRound(humanChoice, computerChoice);
 
+console.log(humanScore, computerScore);
+console.log(typeof(computerChoice));
+console.log(typeof(humanChoice));
